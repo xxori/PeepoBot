0015465fb29a67b98761db1d3bc901e166a5815c
Codian <xxcodianxx@gmail.com>
2019-12-04 11:59:18 +1030

    p u s h

--- extensions/levels.py
+++ extensions/levels.py
@@ -13,7 +13,6 @@ class Leveling(commands.Cog):
     def __init__(self, bot):
         super().__init__()
         self.bot = bot
-        self.level_threshold = 100
 
     @commands.command(brief='View you or a user''s level and exp.', usage='[user]')
     async def level(self, ctx, user:discord.Member = None):
@@ -23,12 +22,12 @@ class Leveling(commands.Cog):
         user_info = await dbcontrol.get_user(user.id)
         level = user_info['level']
         exp = user_info['exp']
+        exp_threshold = user_info['exp_threshold']
 
         embed=discord.Embed(colour=discord.Colour.blurple())
         embed.set_author(name=str(user), icon_url=user.avatar_url)
 
-        exp_required = int(((level+1**2 - level+1)*self.level_threshold)/2)
-        embed.description = f':small_blue_diamond: **Level:** {level}\n:small_orange_diamond: **EXP:** {exp}/{exp_required}**'
+        embed.description = f':small_blue_diamond: **Level:** {level}\n:small_orange_diamond: **EXP:** {exp}/{exp_required}'
         await ctx.send(embed=embed)
 
 def setup(bot):
