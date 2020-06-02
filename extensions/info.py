83bec7e9dfecb4c9282b040e532561c0d6f441b4
Orij56 <55019674+Orij56@users.noreply.github.com>
2020-06-02 20:53:08 +0930

    tidying  info and utils

--- extensions/info.py
+++ extensions/info.py
@@ -24,13 +24,11 @@ SOFTWARE.
 
 import discord
 from discord.ext import commands
-import asyncio
 import datetime
 import aiohttp
 import random
 import utils
 import time
-import re
 import dbcontrol
 import json
 import bs4
@@ -49,47 +47,46 @@ class Utility(commands.Cog):
         embed = discord.Embed(colour=discord.Colour.blurple(), timestamp=datetime.datetime.utcnow())
         embed.set_author(name='Guild Info')
 
-        embed.add_field(name='Name', value=ctx.guild.name, inline=False)
-        embed.add_field(name='Owner', value=str(ctx.guild.owner))
-        embed.add_field(name='Created At', value=ctx.guild.created_at.strftime('%A %d %B %Y at %I:%M %p'), inline=False)
+        embed.add_field(name='Name', value=ctx.guild.name, inline=False) # Name of guild
+        embed.add_field(name='Owner', value=str(ctx.guild.owner)) # User who created guild or has current ownership
+        embed.add_field(name='Created At', value=ctx.guild.created_at.strftime('%A %d %B %Y at %I:%M %p'), inline=False) # Date of guild creation
 
-        botcount = len([m for m in ctx.guild.members if m.bot])
-        humancount = ctx.guild.member_count - botcount
+        botcount = len([m for m in ctx.guild.members if m.bot]) # Amount of bot users in guild, using shorthand for loop
+        humancount = ctx.guild.member_count - botcount # Gets amount of humans by subtracting the amount of bots from total amount
 
         embed.add_field(name='Members', value=f'{ctx.guild.member_count} total, {humancount} humans, {botcount} bots', inline=False)
         embed.add_field(name='Channels', value=f'{len(ctx.guild.text_channels)} text, {len(ctx.guild.voice_channels)} voice', inline=False)
         embed.add_field(name='Roles', value=str(len(ctx.guild.roles)), inline=False)
         embed.add_field(name='Region', value=str(ctx.guild.region).capitalize(), inline=False)
         embed.set_footer(text=ctx.author, icon_url=ctx.author.avatar_url)
-
-
         embed.set_thumbnail(url=ctx.guild.icon_url)
         await ctx.send(embed=embed)
 
     @commands.command(brief='Get information about your Discord account.', description='Shows information about ', usage='[user]')
     async def uinfo(self, ctx, user: discord.Member = None):
+        # If no user is supplied it gives info about the author
         if not user:
             user = ctx.message.author
         embed = discord.Embed(color=user.color if user.color else discord.Colour.blurple(), timestamp=datetime.datetime.utcnow())
         embed.set_author(name=f'User Info')
-        embed.add_field(name='Name', value=str(user), inline=False)
-        embed.add_field(name='ID', value=user.id, inline=False)
-        embed.add_field(name='Highest role', value=user.top_role, inline=False)
-        embed.add_field(name='Joined Server', value=user.joined_at.strftime('%A %d %B %Y at %I:%M %p (UTC)'), inline=False)
-        embed.add_field(name='Account Created', value=user.created_at.strftime('%A %d %B %Y at %I:%M %p (UTC)'), inline=False)
+        embed.add_field(name='Name', value=user, inline=False) # Username
+        embed.add_field(name='ID', value=user.id, inline=False) # Internal identification number
+        embed.add_field(name='Highest role', value=user.top_role, inline=False) # Highest role of the member in current guild
+        embed.add_field(name='Joined Server', value=user.joined_at.strftime('%A %d %B %Y at %I:%M %p (UTC)'), inline=False) # Time the user joined current guild
+        embed.add_field(name='Account Created', value=user.created_at.strftime('%A %d %B %Y at %I:%M %p (UTC)'), inline=False) # Time the user joined discord
         embed.set_footer(text=user, icon_url=user.avatar_url)
         embed.set_thumbnail(url=user.avatar_url)
         if user.nick != None:
+            # If the user has a nickname in current guild it is added
             embed.add_field(name='Nickname: ', value=user.nick, inline=False)
         if user.activity != None:
+            # If the user has a current status it is displayed
             embed.add_field(name='Current Activity: ', value=user.activity.name, inline=False)
         await ctx.send(embed=embed)
 
-    @commands.command(
-        aliases=['ping', 'binfo', 'latency'],
-        brief='Get information about the bot such as latency and uptime.'
-    )
+    @commands.command(aliases=['ping', 'binfo', 'latency'], brief='Get information about the bot such as latency and uptime.')
     async def botinfo(self, ctx):
+        # Subtracts the bots run time in seconds from the current timestamp to get time elapsed
         runtime = datetime.datetime.utcnow() - self.bot.run_time
         uptime = datetime.datetime.utcnow() - self.bot.connect_time
         embed = discord.Embed(colour=discord.Colour.blurple(), timestamp=datetime.datetime.utcnow())
@@ -117,25 +114,34 @@ class Utility(commands.Cog):
         msg = await ctx.send(embed=embed)
         end = time.perf_counter()
         message_receive = int((end - start) * 1000)
+        # Edits embed to add in recieve time
         embed.set_field_at(1, name='Latency', value=latency_text.replace('``...``', f'``{message_receive}ms``', 1))
 
         await msg.edit(embed=embed)
 
+        # Adds in random fun fact or joke after initial information is sent
         if random.randint(1,2) == 1:
             async with aiohttp.ClientSession(loop=self.bot.loop) as session:
+                # A random fact from this cool api
                 response = await session.get(url='https://uselessfacts.jsph.pl/random.json?language=en')
+                # Uses inbuilt aiohttp parser to parse json
                 fact = await response.json()
+                await response.close()
                 await session.close()
             value = fact['text']
             embed.add_field(name='Fact of the Day', value=value, inline=False)
         else:
             async with aiohttp.ClientSession(loop=self.bot.loop) as session:
+                # Gets a random joke from this api with the nsfw jokes blacklisted
                 response = await session.get(url='https://sv443.net/jokeapi/v2/joke/Any?blacklistFlags=nsfw')
                 data = await response.json()
                 await session.close()
+            # If the joke is a two part joke addition work is required
+            # The setup and delivery are both sent on different lines
             if data['type'] == 'twopart':
                 value = data['setup'] + '\n' + data['delivery']
             else:
+                # One-liner
                 value = data['joke']
             embed.add_field(name='Joke of the Day', value=value, inline=False)
         await msg.edit(embed=embed)
@@ -143,12 +149,8 @@ class Utility(commands.Cog):
 
     @commands.command(brief='Shows recently deleted messages')
     async def snipe(self, ctx):
+        # Grabs the list of deleted messages from the bot variable
         snipes_serv = self.bot.snipe_info.get(ctx.guild.id)
-        if snipes_serv is None:
-            await ctx.send(':x: **There was nothing to snipe!**')
-            return 
-
-        now = datetime.datetime.utcnow()
 
         embed = discord.Embed(title=f"Deleted messages from {ctx.message.guild.name}", timestamp=datetime.datetime.utcnow())
         embed.set_footer(text=ctx.author, icon_url=ctx.author.avatar_url)
@@ -156,30 +158,36 @@ class Utility(commands.Cog):
             for msg in snipes_serv:
                 embed.add_field(name=f"{msg.author}", value=msg.content[-300:], inline=False)
             await ctx.send(embed=embed)
+        # No deleted messages exist
         else:
             await ctx.send(':x:**There were no messages to snipe**')
 
     @commands.command(brief='Adds a role to the user, or lists all available roles', usage="[name]")
     async def role(self, ctx, name: str = None):
+        # Grabs the json of existing roles in the guild from the database and parses it into a dictionary
         rolesJSON = (await dbcontrol.get_guild(ctx.guild.id))['roles']
-        rolesDict = json.loads(rolesJSON) if rolesJSON else {}
+        rolesDict = json.loads(rolesJSON) if rolesJSON else {} # If the json doesn't exist in db an empty dict is created
         if name is None:
             if rolesDict == {}:
                 await ctx.send(":x: **No roles currently available**")
             else:
-                await ctx.send(f"**Currently available roles:** ```\n" + "- \n".join(rolesDict.keys()) + "```")
+                # Sends each dictionary key on a new line
+                await ctx.send(f"**Currently available roles:** ```\n" + " \n".join(rolesDict.keys()) + "```")
         else:
             if name not in rolesDict.keys():
                 await ctx.send(f":x: **Role ``{name}`` not found**")
             else:
+                # Role does exist
                 role = ctx.guild.get_role(rolesDict[name])
 
                 if role not in ctx.author.roles:
+                    # Role is above bots top role, can't assign to user
                     if role > ctx.guild.me.top_role:
                         await ctx.send(f":x: **I am not allowed to give you the {name} role**")
                     else:
                         await ctx.author.add_roles(role, reason="Automated role command")
                         await ctx.send(f":thumbsup: **You have been given the role ``{role.name}``**")
+                # If user already has role it is removed from them
                 else:
                     if role > ctx.guild.me.top_role:
                         await ctx.send(f":x: **I am not allowed to remove the {name} role from you**")
@@ -189,26 +197,35 @@ class Utility(commands.Cog):
 
     @commands.command(brief="Creates a new role with specified color", usage="[hex or colour name]", aliases=['color'])
     async def colour(self, ctx, *, colour):
+        # Grabbing existing colors and parsing into a dictionary
         coloursJSON = (await dbcontrol.get_guild(ctx.guild.id))['colours']
         coloursDict = json.loads(coloursJSON)
 
+        # Getting the hex code for the color using the utils function
         colour = utils.colour(colour)
 
         if not colour:
+            # utils.colour returns False if the colour is invalid
             await ctx.send(":x: **Invalid Colour**")
         else:
+            # Checking through each existing colour role to delete it from db if it doesn't exist or remove it from the
+            # user if they have it
             for key in list(coloursDict.keys()):
                 role = ctx.guild.get_role(coloursDict[str(key)])
+                # If the role doesn't exist for some reason its removed from the db
                 if role is None:
                     coloursDict.pop(key)
                     coloursJSON = json.dumps(coloursDict)
                     await dbcontrol.modify_guild(ctx.guild.id, 'colours', coloursJSON)
+                # If the user already has a colour role it is removed
                 if role in ctx.author.roles:
                     await ctx.author.remove_roles(role, reason="Automated colour role removal")
                     await ctx.send("**Your existing colour role was removed**")
 
+            # If a role already exists for chosen colour we don't want to make a new one so the existing role is given
             if str(colour.value) in list(coloursDict.keys()):
                 role = ctx.guild.get_role(coloursDict[str(colour.value)])
+                # If the target colour role is higher than the bots top role then it can't assign it
                 if role > ctx.guild.me.top_role:
                     return await ctx.send(":x: **I don't have permission to give you that role**")
                 if role in ctx.author.roles:
@@ -216,6 +233,7 @@ class Utility(commands.Cog):
                 await ctx.author.add_roles(role, reason="Automated colour command")
                 await ctx.send(f":thumbsup: **You have been given the role ``{colour.value}``**")
 
+            # If role doesn't exist then the new role is created, added to db, and given to the user
             else:
                 role = await ctx.guild.create_role(name=str(colour.value), colour=colour)
                 coloursDict[colour.value] = role.id
@@ -224,6 +242,7 @@ class Utility(commands.Cog):
                 await ctx.author.add_roles(role, reason="Automated colour command")
                 await ctx.send(f":thumbsup: **You have been given the role ``{colour.value}``**")
 
+    # This command sucks ass but it works
     @commands.command(aliases=["dw", "distribution", "distro"], brief="Finds information about a linux distro", usage="<distro>")
     async def distrowatch(self, ctx, *, distro):
         url = "https://distrowatch.com/table.php?distribution="
@@ -256,6 +275,7 @@ class Utility(commands.Cog):
         info = soup.find("table", {"class": "Info"})
         data = []
         rows = info.find_all("tr")
+        # Some dumbshit grabbing different attributes of the distro
         for row in rows:
             cols = row.find_all("td")
             cols = [element.text.strip() for element in cols]
@@ -287,8 +307,11 @@ class Utility(commands.Cog):
                 await session.close()
             if response.status != 200:
                 return await ctx.send(":x: **Invalid comic number**")
+            # Instantiating beautifulsoup object to parse th html
             soup = bs4.BeautifulSoup(html, "html.parser")
+            # Selecting the comic title with css selector
             title = soup.select("#ctitle")[0].text
+            # The second image on the page is the comic image
             img = "https:" + soup.find_all("img")[1].attrs["src"]
             embed = discord.Embed(title=f"#{num}: {title}", timestamp=datetime.datetime.utcnow(), description=f"[Site Link]({url})\n[Image URL]({img})", color=discord.Color.blurple())
             embed.set_image(url=img)
--- utils.py
+++ utils.py
@@ -22,20 +22,23 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
+# This file defines random helper functions
+
 import discord
 from discord.ext import commands
 import aiohttp
 
 import asyncio
 #import pyppeteer as pyp
-from functools import wraps
-import re
-from collections import Counter
+#from functools import wraps
+#import re
+#from collections import Counter
 from datetime import datetime
 import dbcontrol
 import json
 from colour import COLOURS
 
+# Cycle class for the prescence cycle
 class Cycle:
     def __init__(self, elements):
         self.elements = elements
@@ -73,16 +76,14 @@ def format_thousands(num):
     else:
         return num
 
+# Checks if any given user is a bot developer using the developer ids from the config
 async def is_developer(ctx):
     if not ctx.bot.check_dev(ctx.message.author.id):
         raise commands.NotOwner
     return True
 
+# Formats large numbers into human readable numbers, such as 1000 to 1,000
 def punctuate_number(number, div=','):
-    '''
-    1234 -> 1,234
-    123456789 ->
-    '''
     out = ''
     c = 0
     for char in reversed(str(number)):
@@ -97,7 +98,7 @@ class HierarchyPermissionError(commands.CommandError):
     def __init__(self, ctx, target):
         super().__init__('', [ctx, target])
 
-# copied over from bartender
+# copied over from bartender, formats timestamps into human readable text
 def strfdelta(tdelta, fmt):
     delta = {"D": tdelta.days}
     delta['H'], remainder = divmod(tdelta.seconds, 3600)
@@ -122,20 +123,11 @@ async def in_voice_channel(ctx):
     else:
         raise NotInSameVoiceChannel
 
-# Checks if the given argument is a hexadecimal number
-def hex(num: str):
-    try:
-        hex = int(num, base=16)
-        if hex < 0:
-            return None
-        if len(num) > 6:
-            return None
-        return hex
-    except:
-        return None
-
+# The main check loop for the bot, checking for expired tempmutes and colour roles that no users own
 async def check(bot):
+    # Runs forever while bot is running
     while bot.initialization_finished and bot.is_ready():
+        # Does check for every guild
         for guild in bot.guilds:
             guildid = guild.id
             now = datetime.utcnow().timestamp()
@@ -146,17 +138,20 @@ async def check(bot):
 
             muteroleid = await dbcontrol.get_setting(guildid,'muterole')
             muterole = guild.get_role(muteroleid)
+            # Checking for expired tempmutes
             for i in list(mutesDict.keys()):
                 if mutesDict[i] < now:
                     mutesDict.pop(i)
                     user = guild.get_member(int(i))
                     if muterole:
                         await user.remove_roles(muterole, reason="Tempmute expired")
+            # Commits changes to db
             mutesJSON = json.dumps(mutesDict)
             await dbcontrol.modify_guild(guildid, 'tempmutes', mutesJSON)
 
             coloursJSON = (await dbcontrol.get_guild(guildid))['colours']
             coloursDict = json.loads(coloursJSON)
+            # Checks for colour roles which no users own, which are deleted
             for colour in list(coloursDict.keys()):
                 used = False
                 role = guild.get_role(coloursDict[colour])
@@ -173,6 +168,8 @@ async def check(bot):
             await dbcontrol.modify_guild(guildid, 'colours', coloursJSON)
             await asyncio.sleep(30)
 
+# Converts a colour string to a discord.Colour object. If the string is hex, nothing is changed. If the string is a
+# colour name it uses the COLOURS dict found in colour.py
 def colour(colour):
     colourHex = hex(colour.replace("#", ""))
     if colourHex != None:
@@ -182,6 +179,18 @@ def colour(colour):
     else:
         return False
     return colour
+
+# Converts string to hexadecimal, or returns None if it is invalid
+def hex(num: str):
+    try:
+        hex = int(num, base=16)
+        if hex < 0:
+            return None
+        if len(num) > 6:
+            return None
+        return hex
+    except:
+        return None
 
 class NotPlayingAudio(commands.CheckFailure):
     def __init__(self):
